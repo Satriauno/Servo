736c0410868cd98b2b74dd37d431248ff8a87658
Satriauno <68578408+Satriauno@users.noreply.github.com>
2020-07-23 14:18:29 +0700

    project 2
    Bahan belajar project 2 membuat servo dengan menyambungkan ke Blynk dan mencoding melalui Arduino ide

--- Servo.ino
+++ Servo.ino
@@ -1,61 +1,36 @@
-/*************************************************************
-  Download latest Blynk library here:
-    https://github.com/blynkkk/blynk-library/releases/latest
+#define BLYNK_PRINT Serial 
+//Definisi Serial pada Blink 
 
-  Blynk is a platform with iOS and Android apps to control
-  Arduino, Raspberry Pi and the likes over the Internet.
-  You can easily build graphic interfaces for all your
-  projects by simply dragging and dropping widgets.
+//Setting Library 
+#include <ESP8266WiFi.h> 
+#include <BlynkSimpleEsp8266.h> 
+#include <Servo.h> 
 
-    Downloads, docs, tutorials: http://www.blynk.cc
-    Sketch generator:           http://examples.blynk.cc
-    Blynk community:            http://community.blynk.cc
-    Follow us:                  http://www.fb.com/blynkapp
-                                http://twitter.com/blynk_app
+//Definisi variabel token, SSID, password 
+char auth[] = "Your Auth token "; 
+char ssid[] = "Your wifi"; 
+char pass[] = "Your wifi password"; 
 
-  Blynk library is licensed under MIT license
-  This example code is in public domain.
+//Definisi fungsi servo menjadi gerak_servo 
+Servo gerak_servo; 
 
- *************************************************************
-
-  Rotate a servo using a slider!
-
-  App project setup:
-    Slider widget (0...180) on V3
- *************************************************************/
-
-/* Comment this out to disable prints and save space */
-#define BLYNK_PRINT Serial
-
-
-#include <SPI.h>
-#include <Ethernet.h>
-#include <BlynkSimpleEthernet.h>
-#include <Servo.h>
-
-// You should get Auth Token in the Blynk App.
-// Go to the Project Settings (nut icon).
-char auth[] = "YourAuthToken";
-
-Servo servo;
-
-BLYNK_WRITE(V3)
-{
-  servo.write(param.asInt());
+//Fungsi Blynk untuk mengatur nilai servo dengan pin virtual V1 
+BLYNK_WRITE(V1){ 
+  gerak_servo.write(param.asInt()); 
 }
 
-void setup()
-{
-  // Debug console
-  Serial.begin(9600);
-
-  Blynk.begin(auth);
-
-  servo.attach(9);
+//Settingan awal default 
+void setup(){ 
+ Serial.begin(9600);
+ //memulai serial monitor dengan baudrate 9600 
+  
+ gerak_servo.attach(2); 
+ //setting pin gpio 2 ke servo 
+  
+ Blynk.begin(auth, ssid, pass); 
+ //setting blynk 
+} 
+void loop(){ 
+  Blynk.run(); 
+  //memulai blynk 
 }
-
-void loop()
-{
-  Blynk.run();
-}
-
